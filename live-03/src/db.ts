17537072cc8b48de600c051e749e16a04e5c16ce
Jorge <jorgeauri@gmail.com>
2021-10-07 21:19:33 -0300

    finalizado

--- live-03/src/db.ts
+++ live-03/src/db.ts
@@ -1,7 +1,6 @@
 import { Pool } from "pg";
 
-const connectionString =
-  "postgres://uadjgcxh:R1Ya8OeB536d8Bb5dlguuLWTR5ltFM0d@tuffi.db.elephantsql.com/uadjgcxh";
+const connectionString = "";
 
 const db = new Pool({ connectionString });
 
